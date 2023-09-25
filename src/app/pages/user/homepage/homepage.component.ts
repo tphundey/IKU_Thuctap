cfca96556050aba0f53b72c850e5fd748a1337c9
Your Name <youremail@yourdomain.com>
2023-09-25 17:35:00 +0700

    delete carousel products

--- src/app/pages/user/homepage/homepage.component.ts
+++ src/app/pages/user/homepage/homepage.component.ts
@@ -7,22 +7,5 @@ import { Component } from '@angular/core';
 })
 export class HomepageComponent {
  
-  products = [
-    { name: 'Sản phẩm 1', description: 'Mô tả sản phẩm 1', imageUrl: 'https://307a0e78.vws.vegacdn.vn/view/v2/image/img.book/0/0/1/40052.jpg?v=1&w=200&h=292' },
-    { name: 'Sản phẩm 2', description: 'Mô tả sản phẩm 2', imageUrl: 'https://307a0e78.vws.vegacdn.vn/view/v2/image/img.book/0/0/1/40052.jpg?v=1&w=200&h=292' },
-    { name: 'Sản phẩm 3', description: 'Mô tả sản phẩm 3', imageUrl: 'https://307a0e78.vws.vegacdn.vn/view/v2/image/img.book/0/0/1/40052.jpg?v=1&w=200&h=292' },
-    { name: 'Sản phẩm 1', description: 'Mô tả sản phẩm 1', imageUrl: 'https://307a0e78.vws.vegacdn.vn/view/v2/image/img.book/0/0/1/40052.jpg?v=1&w=200&h=292' },
-    { name: 'Sản phẩm 2', description: 'Mô tả sản phẩm 2', imageUrl: 'https://307a0e78.vws.vegacdn.vn/view/v2/image/img.book/0/0/1/40052.jpg?v=1&w=200&h=292' },
-    { name: 'Sản phẩm 3', description: 'Mô tả sản phẩm 3', imageUrl: 'https://307a0e78.vws.vegacdn.vn/view/v2/image/img.book/0/0/1/40052.jpg?v=1&w=200&h=292' },
-  ];
-  groupedProducts = this.groupArray(this.products, 4);
-
-  // Hàm chia mảng thành các nhóm
-  groupArray(array: any[], groupSize: number) {
-    const grouped = [];
-    for (let i = 0; i < array.length; i += groupSize) {
-      grouped.push(array.slice(i, i + groupSize));
-    }
-    return grouped;
-  }
+ 
 }
