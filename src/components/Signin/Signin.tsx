db7e2d893ff64c22042e6d64bb991f8610660352
Your Name <taidvph20044@gmail.com>
2023-11-02 13:47:40 +0700

    Add Firebase to dependencies and update Google sign-in method

--- src/components/Signin/Signin.tsx
+++ src/components/Signin/Signin.tsx
@@ -1,146 +1,81 @@
-import "./Signin.css"
-import { useState, useEffect } from 'react';
-import { GoogleLogin } from 'react-google-login';
-import { gapi } from 'gapi-script'
-import { useNavigate } from 'react-router-dom';
+import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
 import { ToastContainer, toast } from 'react-toastify';
-import axios from "axios";
 import 'react-toastify/dist/ReactToastify.css';
-const clientId = "617522400337-v8petg67tn301qkocslk6or3j9c4jjmn.apps.googleusercontent.com"
+import { useState, useEffect } from 'react';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1EWRdSA6tMWHHB-2nHwljjQIGDL_-x_E",
+  authDomain: "course23-c0a29.firebaseapp.com",
+  projectId: "course23-c0a29",
+  storageBucket: "course23-c0a29.appspot.com",
+  messagingSenderId: "1090440812389",
+  appId: "1:1090440812389:web:e96b86b4d952f89c0d738c",
+  measurementId: "G-51L48W6PCB"
+};
+
+// Khởi tạo ứng dụng Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 const Signin = () => {
-
-  const navigate = useNavigate();
-  const onSuccess = (res: any) => {
-    const emailToCheck = "anhnek033@gmail.com";
-    const profile = {
-      email: res.profileObj.email,
-      name: res.profileObj.name,
-      img: res.profileObj.imageUrl
-    };
-    // Chuyển đổi object profile thành chuỗi JSON
-    const profileJSON = JSON.stringify(profile);
-
-    console.log("Login Success", res.profileObj);
-    localStorage.setItem('isLoggedIn', 'true');
-    localStorage.setItem('profile', profileJSON);
-
-
-    // Kiểm tra email trùng trước khi gửi yêu cầu POST
-    axios.get(`http://localhost:3000/googleAccount?email=${res.profileObj.email}`)
-      .then(response => {
-        if (response.data.length > 0) {
-          console.log('có tài khoản rồi');
-        } else {
-          axios.post('http://localhost:3000/googleAccount', profile)
-            .then(response => {
-              console.log('Post thông tin tài khoản thành công !');
-            })
-            .catch(error => {
-              console.log(error);
-            });
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      });
-
-    toast.success('Đăng nhập thành công!', {
-      className: 'thongbaothanhcong',
-      position: toast.POSITION.TOP_CENTER,
-      autoClose: 2000,
-    });
-
-    if (res.profileObj.email === emailToCheck) {
-      setTimeout(() => {
-        navigate('/admin');
-      }, 3000);
-    } else {
-      setTimeout(() => {
-        navigate('/');
-        location.reload();
-      }, 3000);
-    }
-  }
-
-  const onFailure = (res: any) => {
-    console.log("Login Fail", res);
-    alert('Không thành công')
-  }
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    function start() {
-      gapi.client.init({
-        clientId: clientId,
-        scope: ""
-      })
-    };
-    gapi.load('client:auth2', start)
-  })
+    // Sử dụng onAuthStateChanged để kiểm tra trạng thái xác thực của người dùng
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        // Người dùng đã đăng nhập
+        setUser(currentUser);
+      } else {
+        // Người dùng chưa đăng nhập
+        setUser(null);
+      }
+    });
+
+    return () => {
+      // Hủy đăng ký sự kiện khi component unmount
+      unsubscribe();
+    }
+  }, [auth]);
+
+  const googleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      await signInWithPopup(auth, provider);
+      // Không cần lưu thông tin người dùng lên local, người dùng đã được xác thực bởi Firebase
+      toast.success('Đăng nhập thành công!', {
+        className: 'thongbaothanhcong',
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 2000,
+      });
+    } catch (error) {
+      console.error('Authentication failed:', error);
+      alert('Không thành công');
+    }
+  };
 
   return (
     <div className="container_signin">
-      <div className="wrapper_signin">
-        <div className="signin_left">
-          <div className="img">
-            <img src="../../../public/img/img_signin.jpg" alt="" />
-          </div>
+      {/* Rest of your JSX code here */}
+      {user ? (
+        <div>
+          <p>Xin chào, {user.displayName}!</p>
+          <button onClick={() => auth.signOut()}>Đăng xuất</button>
         </div>
-        <div className="signin_right">
-          <div className="title_signin">
-            <h3>Đăng nhập</h3>
-            <p>Hãy đăng nhập để được hưởng đặc quyền riêng dành cho bạn</p>
-          </div>
-          <form action="" className="form_login">
-            <div className="controll">
-              <label className="tk_pas">TÀI KHOẢN *</label> <br />
-              <input type="text" />
-            </div>
-            <div className="controll">
-              <label className="tk_pas">MẬT KHẨU *</label> <br />
-              <input type="text" />
-            </div>
-            <div className="luu">
-              <input type="radio" name="" id="" />
-              <div className="text_luu">
-                Lưu tài khoản
-              </div> <br />
-            </div>
-            <button className="btn-signin">Đăng nhập</button>
-            <br />
-            <div className="passs">
-
-              <a href="" className="quen_pass">Quên mật khẩu</a>
-            </div>
-            <br />
-            <button className="signin_facebook">
-              <div className="icon"></div>
-              <div className="text">Đăng nhập bằng facebook</div>
-            </button>
-            <br />
-            <button className="signin_google">
-              <div className="icon"></div>
-              <GoogleLogin
-                clientId={clientId} // Thay thế YOUR_GOOGLE_CLIENT_ID bằng Client ID của bạn
-                buttonText="Đăng nhập bằng Google"
-                onSuccess={onSuccess}
-                onFailure={onFailure}
-                cookiePolicy={'single_host_origin'}
-                isSignedIn={true}
-              />
-
-            </button>
-          </form>
-          <div className="signin_left-bottom">
-            <p>Bạn chưa có tài khoản</p>
-            <a href="./signup" className="dangkingay">Đăng kí ngay</a>
-          </div>
-        </div>
-      </div>
-      {/* Component ToastContainer để hiển thị thông báo */}
+      ) : (
+        <button className="signin_google" onClick={googleSignIn}>
+          <div className="icon"></div>
+          Đăng nhập bằng Google
+        </button>
+      )}
       <ToastContainer />
     </div>
   );
 };
 
 export default Signin;
+
+
+
+
