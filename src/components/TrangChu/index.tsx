2a4f2bbccea3b48811d2aa1d77fea91ca2e63b17
Your Name <taidvph20044@gmail.com>
2023-11-06 20:24:31 +0700

    Update CSS and add responsive design for TrangChu component

--- src/components/TrangChu/index.tsx
+++ src/components/TrangChu/index.tsx
@@ -7,7 +7,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Carousel } from 'antd';
-
+import './res.css'
 const Home = () => {
 
     const settings1 = {
@@ -17,7 +17,13 @@ const Home = () => {
         slidesToShow: 5, // Hiển thị 4 sản phẩm trên mỗi dòng
         slidesToScroll: 3,
     };
-
+    const settings1res = {
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 2, // Hiển thị 4 sản phẩm trên mỗi dòng
+        slidesToScroll: 3,
+    };
     const settings2 = {
         arrows: true,
         infinite: true,
@@ -35,7 +41,7 @@ const Home = () => {
 
     return (
         <div>
-            <div className="container">
+            <div className="container responsive-div">
                 <div className="list-tc">
                     <Slider {...settings2}>
                         <div>
@@ -74,6 +80,62 @@ const Home = () => {
                     </div>
                     <div>
                         <Slider {...settings1}>
+                            {products.map((item: any) => (
+                                <div className="product text-center p-6">
+                                    <Link to={`/products/${item.id}`} >
+                                        <div className="image">
+                                            <img src={item.img} alt="" />
+                                        </div>
+                                        <div className="name mt-2">{item.name}</div>
+                                        <div className="price">{item.price}.000đ</div>
+                                    </Link>
+                                </div>
+                            ))}
+                        </Slider>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="mt-4 formo">
+                <div className="list-tc">
+                    <Slider {...settings2}>
+                        <div>
+                            <img src="https://theme.hstatic.net/200000343865/1001052087/14/ms_banner_img1.jpg?v=411" alt="" />
+                        </div>
+                        <div>
+                            <img src="https://theme.hstatic.net/200000343865/1001052087/14/ms_banner_img1.jpg?v=411" alt="" />
+                        </div>
+                        <div>
+                            <img src="https://theme.hstatic.net/200000343865/1001052087/14/ms_banner_img1.jpg?v=411" alt="" />
+                        </div>
+                        <div>
+                            <img src="https://theme.hstatic.net/200000343865/1001052087/14/ms_banner_img1.jpg?v=411" alt="" />
+                        </div>
+                    </Slider>
+                    <div className="mockup-code mt-4 mb-4">
+                        <pre data-prefix="$"><code>npm i Sản phẩm nổi bật!</code></pre>
+                    </div>
+                    <div>
+                        <Slider {...settings1res}>
+                            {products.map((item: any) => (
+                                <div className="product text-center p-6">
+                                    <Link to={`/products/${item.id}`} >
+                                        <div className="image">
+                                            <img src={item.img} alt="" />
+                                        </div>
+                                        <div className="name mt-2">{item.name}</div>
+                                        <div className="price">{item.price}.000đ</div>
+                                    </Link>
+                                </div>
+                            ))}
+                        </Slider>
+                    </div>
+                    <div className="mockup-code mt-4 mb-4">
+                        <pre data-prefix="$"><code>npm i Sản phẩm khuyến mãi!</code></pre>
+                    </div>
+                    <div>
+                        <Slider {...settings1res}>
                             {products.map((item: any) => (
                                 <div className="product text-center p-6">
                                     <Link to={`/products/${item.id}`} >
